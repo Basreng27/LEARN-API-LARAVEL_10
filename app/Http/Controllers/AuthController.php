ac2dfb10219dfacb402c597ec117bece526cb365
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-08-06 09:46:54 +0700

    DONE : API Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // untuk validasi data inputan
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $account = Account::where('username', $request->username)->first();
+
+        if (!$account || !Hash::check($request->password, $account->password)) {
+            throw ValidationException::withMessages(([
+                'username' => ['Your Account Is Not Registered']
+            ]));
+        }
+
+        return $account->createToken('user logged')->plainTextToken;
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+    }
+
+    public function logged(Request $request)
+    {
+        // get data yang sedang login
+        return response()->json(Auth::user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KomikController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/komik', [KomikController::class, 'index']);
-Route::get('/komik/{id}', [KomikController::class, 'detail']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
+
+Route::get('/komik', [KomikController::class, 'index'])->middleware(['auth:sanctum']); //sanctum fungsinya cek apakah user login atau belum
+Route::get('/komik/{id}', [KomikController::class, 'detail'])->middleware(['auth:sanctum']);
+
+Route::get('/logged', [AuthController::class, 'logged'])->middleware(['auth:sanctum']);
